aada182deeec08cb5f6c6fa21feb786bf785ffc0
Gerardo <Gerardo@MacBook-Pro-de-93477.local>
2021-01-08 14:44:25 -0600

    changes to config.yaml, url verification, and split into different files

--- main.py
+++ main.py
@@ -17,10 +17,17 @@ base_url = 'https://10.48.245.2:9182'
 VD_IP = "10.48.245.2"
 #url = 'https://10.48.245.2:9182/api/config/nms/provider/analytics-cluster'
 
+class AuthError(Exception):
+	pass
+
+class URLError(Exception):
+	pass
 
 def get_data(url):
-        raw = requests.get(url, auth=(user, password), headers={"Accept": "application/json"}, verify=False)
-        return raw     
+		raw = requests.get(url, auth=(user, password), headers={"Accept": "application/json"}, verify=False)
+		if raw.status_code != 200:
+			raise URLError
+		return raw     
 
 def check_ping():
     response = os.system("ping -c 1 -W 500 " + VD_IP+ " > nul")
@@ -54,36 +61,57 @@ def get_url(arg):
 def create_file(querytype, sitename):
 	dateTimeObj = datetime.now()
 	timestamp = str(dateTimeObj.year) + str(dateTimeObj.month) + str(dateTimeObj.day) + '-' + str(dateTimeObj.hour) + str(dateTimeObj.minute) + str(dateTimeObj.second) + '-'
-	print(timestamp)
-	filename = "output10/" + timestamp + querytype + "-" + sitename + ".txt"
+	#print(timestamp)
+	filename = "output/" + timestamp + querytype + "-" + sitename + ".txt"
 	outF = open(filename, "w")
 	print("SITE: " + sitename, file=outF)
 	print("QUERY TYPE: " + querytype, file=outF)
 	outF.close()
 	return filename
 
-def read_yaml():
+def print_to_file(file, text):
+	outF = open(filename, "a")	
+	print(text, file=outF)
+	outF.close()
+
+def read_yaml(path):
 	basic_route_url = base_url + '/api/operational/devices/device/SITE/live-status/rib/org/org/ORG/routing-instance-name/RTINAME,ipv4/route-entry-summary'
 	url_list = []
-	with open(r'config.yaml') as file:
+	with open(path) as file:
 		# The FullLoader parameter handles the conversion from YAML
 		# scalar values to Python the dictionary format
 		config = yaml.load(file, Loader=yaml.FullLoader)
-		for sites in config.get('sites'):
-			site = list(sites.keys())[0]
-			for orgs in  list(sites.values()):
-				for org in orgs:
-					orgname = list(org.keys())[0]
-					rti_list = list(org.values())[0]
-					for rti in rti_list:
-						print(rti)
-						print(site + ' ' + orgname + ' ' + rti)
-						url = basic_route_url.replace("SITE", site)
-						url = url.replace("ORG", orgname)
-						url = url.replace("RTINAME", rti)
-						url_list.append([site, rti, url])
-						print()
-	return url_list
+	return config
+
+def read_config():
+	config = read_yaml('config.yaml')
+	#print(config)
+	return config
+
+def read_routes():
+	basic_route_url = base_url + '/api/operational/devices/device/SITE/live-status/rib/org/org/ORG/routing-instance-name/RTINAME,ipv4/route-entry-summary'
+	url_list = []
+	sites_check_list = []
+	routes = read_yaml('routes.yaml')
+	for sites in routes.get('sites'):
+		site = list(sites.keys())[0]
+		#print(site)
+		url_list_test = []
+		for orgs in  list(sites.values()):
+			for org in orgs:
+				orgname = list(org.keys())[0]
+				rti_list = list(org.values())[0]
+				for rti in rti_list:
+					#print(rti)
+					#print(site + ' ' + orgname + ' ' + rti)
+					url = basic_route_url.replace("SITE", site)
+					url = url.replace("ORG", orgname)
+					url = url.replace("RTINAME", rti)
+					url_list.append([site, rti, url])
+					url_list_test.append([rti,url,orgname])
+		sites_check_list.append([site,url_list_test])
+	#print(sites_check_list)
+	return sites_check_list
 
 #def get_site_names(json_text):
 #	json_object = json.loads(json_text.text)
@@ -93,23 +121,43 @@ def read_yaml():
 #			print(key.get('name'))
 #			print(key.get('ipAddress'))
 #print(routes())
+config = read_config()
+user = config['user']
+password = config['password']
+VD_IP = config['director-ip']
 
-url_list = read_yaml()
-for url1 in url_list:
-	print(url1[2])
+print('Director IP: ' + VD_IP + "    User: " + user + "    password: " + password)
+#base_url = 'https://10.48.245.2:9182'
+
+base_url = 'https://' + VD_IP + ':9182'
+
 ping = check_ping()
 if ping:
-	filename = create_file("Routes","MEO-GW-001")
 	url = get_url(sites())
-	sites = get_data(url)
-	#print(sites.text)
-	Site.get_site_names(sites)
-	for url1 in url_list:
-		#print(url1[2])
-		routes = get_data(url1[2])
-		print()
-		print("SITE: " + url1 [0])
-		RoutingTable.print_routes_to_file(routes, url1[1], filename)
+	try:
+		sites = get_data(url)
+		#print(sites.text)
+		Site.get_site_names(sites)
+		routes_site_list = read_routes()
+		for site in routes_site_list:
+			sitename = site[0]
+			url_list = site[1]
+			print()
+			print("SITE: " + sitename)
+			filename = create_file("Routes",sitename)
+			for url1 in url_list:
+				print("!")
+				print(url1)
+				#print(url1[2])
+				try:
+					routes = get_data(url1[1])
+					RoutingTable.print_routes_to_file(routes, url1[0], filename)
+				except URLError:
+					pdata = "Data received Site: " + sitename + " Org: "+ url1[2] + " RTI: " + url1[0] +  ", is Incorrect. Please check route.yaml file"
+					print(pdata + "/n")
+					print_to_file(filename, pdata)
+	except AuthError:
+		print("Incorrect Authentication. Please check config.yaml file")
 else:
 	print("VD not reachable")
 
