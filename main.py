50986ccff263776f0c823fbbdfda7799f3caec9d
Gerardo Melesio <gerardo@versa-networks.com>
2021-09-09 18:51:30 -0500

    first cmd version

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import requests
+#!/usr/bin/env python
+
+import cmd
 import getpass
 #import sites_list
 import json
@@ -6,6 +8,11 @@ import jsondiff
 import urllib3
 import os
 import yaml
+import requests
+import click
+
+#from clint import resources
+#from clint import arguments
 from datetime import datetime
 #from jsndiff import diff
 from files import File
@@ -13,19 +20,57 @@ from sites import Site
 from routes import Route, RoutingTable
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+
+#@click.command()
+#@click.option('--save', default=1, help='Number of greetings.')
+#@click.option('--user', prompt='Your name', help='The person to greet.')
+
+
+class VsnapyShell(cmd.Cmd):
+	intro = 'Welcome to the Vsnapy Shell.   Type help or ? to list commands.\n'
+	prompt = '(vsnapy) '
+	file = None
+
+	def do_list(self, arg):
+		'Show the list of sites attached to Director (ALL Orgs)'
+		url = base_url + '/vnms/cloud/systems/getAllAppliancesBasicDetails?offset=0&limit=20'
+		sites = get_data(url,user,password)
+		Site.get_site_names(sites)
+		#tries to fetch the list of sites the Director has. If the request has a 400 error it assumes it is an Auth error (could be something else though, prob need to think this through)
+		#print(url)
+
+	def do_exit(self, arg):
+		'Exit Vsnapy Shell'
+		print('Thank you for using Vsnapy')
+		self.close()
+		exit()
+		return True
+
+	def do_save_routes(self,arg):
+		'Save routes to json and text files'
+		save_routes()
+
+	def close(self):
+		if self.file:
+			self.file.close()
+			self.file = None
+
 class AuthError(Exception):
 	pass
 
 class URLError(Exception):
 	pass
 
-def get_data(url):
+class VD_NotReachable(Exception):
+	pass
+
+def get_data(url,user,password):
 		raw = requests.get(url, auth=(user, password), headers={"Accept": "application/json"}, verify=False)
-		if raw.status_code != 200:
-			raise URLError
+		if raw.status_code == 401:
+			raise AuthError
 		return raw     
 
-def check_ping():
+def check_ping(VD_IP):
     response = os.system("ping -c 2 -W 500 " + VD_IP)
     # and then check the response...
     if response == 0:
@@ -69,53 +114,65 @@ def read_routes(base_url):
 		sites_check_list.append([site,url_list_test])
 	return sites_check_list
 
+def save_routes():
+	try:
+			routes_site_list = read_routes(base_url)
+			for site in routes_site_list:
+				sitename = site[0]
+				url_list = site[1]
+				print()
+				print("SITE: " + sitename)
+				filename_txt, filename_json = File.create_files("Routes",sitename)
+				for url1 in url_list:
+					print("!")
+					print(url1)
+					#tries to fetch the rti. If it receives no respeonse, it asumes incorrect information taken from the .yaml file (could be something else though, prob need to think this through)
+					try:
+						routes = get_data(url1[1],user,password)
+						RoutingTable.print_routes_to_file(routes, url1, filename_txt)
+						RoutingTable.save_routes_to_json_file(routes,url1,sitename,filename_json)
+					except URLError:
+						pdata = "Data received Site: " + sitename + " Org: "+ url1[2] + " RTI: " + url1[0] +  ", is Incorrect. Please check route.yaml file"
+						#print(pdata + "/n")
+						#print_to_file(filename, pdata)
+	except AuthError:
+		print("Incorrect Authentication. Please check the user and password")
+
 def read_config():
 	config = read_yaml('config.yaml')
 	#print(config)
 	return config
 
-#Begining of the main code
-# This pulls the information from the config.yaml file and takes the inputs
-#user = config['user']
-#password = config['password']
-config = read_config()
-VD_IP = config['director-ip']
-#Creates a base URL that all API calls will use
-base_url = 'https://' + VD_IP + ':9182'
-#Checks reachability to Director
-ping = check_ping()
-if ping:
-    user = input("Username:")
-    password = getpass.getpass(prompt='Password for "'+ user +':')
-    url = base_url + '/vnms/cloud/systems/getAllAppliancesBasicDetails?offset=0&limit=20'
-    #tries to fetch the list of sites the Director has. If the request has a 400 error it assumes it is an Auth error (could be something else though, prob need to think this through)
-    #print(url)
-    try:
-        sites = get_data(url)
-        Site.get_site_names(sites)
-        routes_site_list = read_routes(base_url)
-        for site in routes_site_list:
-            sitename = site[0]
-            url_list = site[1]
-            print()
-            print("SITE: " + sitename)
-            filename_txt, filename_json = File.create_files("Routes",sitename)
-            for url1 in url_list:
-                print("!")
-                print(url1)
-				#tries to fetch the rti. If the request has a 400 error it assumes the url has incorrect information taken from the .yaml file (could be something else though, prob need to think this through)
-                try:
-                    routes = get_data(url1[1])
-                    RoutingTable.print_routes_to_file(routes, url1, filename_txt)
-                    RoutingTable.save_routes_to_json_file(routes,url1,sitename,filename_json)
-                except URLError:
-                    pdata = "Data received Site: " + sitename + " Org: "+ url1[2] + " RTI: " + url1[0] +  ", is Incorrect. Please check route.yaml file"
-                    print(pdata + "/n")
-                    print_to_file(filename, pdata)
-    except AuthError:
-        print("Incorrect Authentication")
-    except URLError:
-        print("Incorrect Authentication")
-else:
-    print("VD not reachable")
+def main():
+	#Begining of the main code
+	# This pulls the information from the config.yaml file and takes the inputs
+	#user = config['user']
+	#password = config['password']
+	config = read_config()
+	global VD_IP
+	VD_IP = config['director-ip']
+	#Creates a base URL that all API calls will use
+	global base_url
+	base_url = 'https://' + VD_IP + ':9182'
+	#Checks reachability to Director
+	ping = check_ping(VD_IP)
+	if ping:
+		global user
+		user = input("Username:")
+		global password
+		password = getpass.getpass(prompt='Password for "'+ user +':')
+		url = base_url + "/vnms/tasks/summary"
+		raw = get_data(url,user,password)
+		#print(raw)
+	else:
+		print("VD not reachable")
+		raise VD_NotReachable
 
+if __name__ == "__main__":
+	try:	
+		main()
+		VsnapyShell().cmdloop()
+	except VD_NotReachable:
+		print("IP for Versa Director in YAML file is not reachable. Please check")
+	except AuthError:
+		print("Incorrect Authentication. Please check the user and password")
